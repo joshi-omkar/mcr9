71c734ae408564c88f11b21b33b3acb347ec9a7b
joshi-omkar <joshiomkar0400@gmail.com>
2023-07-28 20:46:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {Router, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      </Router>
+    </div>
+  )
+}
+
+export default App
